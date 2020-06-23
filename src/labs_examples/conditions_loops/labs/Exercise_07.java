e11d45dc45f8d2ef39b539d4721bad4addeac8e9
unknown <Journeyjava0@gmail.com>
2020-06-22 23:16:41 -0600

    uploading Exercise 07_loops

--- src/labs_examples/conditions_loops/labs/Exercise_07.java
+++ src/labs_examples/conditions_loops/labs/Exercise_07.java
@@ -1,17 +1,50 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 7: First vowel
- *
- *      Take in a word from the user and using a "while" loop, find the first vowel in the word.
- *      Once you find the vowel, print out the word and the first vowel.
- *
- *      Hints:
- *          - there are a few helpful methods in the String class called length(), charAt() and indexOf()
- *          - you'll likely want to use a String that contains all the vowels:
- *              - ie: String vowels = "aeiou";
- *
+ * <p>
+ * Take in a word from the user and using a "while" loop, find the first vowel in the word.
+ * Once you find the vowel, print out the word and the first vowel.
+ * <p>
+ * Hints:
+ * - there are a few helpful methods in the String class called length(), charAt() and indexOf()
+ * - you'll likely want to use a String that contains all the vowels:
+ * - ie: String vowels = "aeiou";
  */
 
 public class Exercise_07 {
+    public static void main(String[] args) {
+        //Step 1: Get user input using scanner & assign to String userInput
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word ");
+        String userInput = scanner.nextLine();
+        String vowels = "aeiou";
+        int i = 0;
+
+        while (i <= userInput.length() + 1) {
+            if (userInput.charAt(i) == vowels.charAt(0)) {
+                System.out.println("first vowel is " + userInput.charAt(i) + " found at index " + i + " of word " + userInput);
+                break;
+            } else if (userInput.charAt(i) == vowels.charAt(1)) {
+                System.out.println("first vowel is " + userInput.charAt(i) + " found at index " + i + " of word " + userInput);
+                break;
+            } else if (userInput.charAt(i) == vowels.charAt(2)) {
+                System.out.println("first vowel is " + userInput.charAt(i) + " found at index " + i + " of word " + userInput);
+                break;
+            } else if (userInput.charAt(i) == vowels.charAt(3)) {
+                System.out.println("first vowel is " + userInput.charAt(i) + " found at index " + i + " of word " + userInput);
+                break;
+            } else if (userInput.charAt(i) == vowels.charAt(4)) {
+                System.out.println("first vowel is " + userInput.charAt(i) + " found at index " + i + " of word " + userInput);
+                break;
+            }
+            i++;
+        }
+    }
 }
+
+
+
+

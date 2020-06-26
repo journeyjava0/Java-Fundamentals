37a7736be2497d733dd6d6ef68d39106ee4046dd
unknown <Journeyjava0@gmail.com>
2020-06-25 19:58:13 -0600

    final update

--- src/labs_examples/conditions_loops/labs/Exercise_06.java
+++ src/labs_examples/conditions_loops/labs/Exercise_06.java
@@ -12,11 +12,12 @@ public class Exercise_06 {
     public static void main(String[] args) {
         int i = 0;
         int sum = 0;
+
         while (i <= 100) {
             sum += i;
            i++;
-        } System.out.println(sum);
+        }
 
+        System.out.println(sum);
     }
-
 }

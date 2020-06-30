99bebf8d82b4801aef91823c1c2317153431edfd
unknown <Journeyjava0@gmail.com>
2020-06-29 20:43:57 -0600

    uploading exercise 2

--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -1,5 +1,9 @@
 package labs_examples.arrays.labs;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 /**
  *  More labs_examples.arrays
  *
@@ -14,8 +18,27 @@ public class Exercise_02 {
 
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
-        // write code here
+        //convert int[] array to an array list
+        List<Integer> vals = new ArrayList<>();
 
+        //populate the array list with the values of int[] array
+        for (int i : array) {
+            vals.add(i);
+        }
 
+        // scanner declaration
+        System.out.println("Please enter a number from 1 - 10: ");
+        Scanner input = new Scanner(System.in);
+        int nums = input.nextInt();
+
+        //did the user enter valid input?
+        if (nums >= 0 && nums <=10) {
+            //if yes then determine and return the element of the index
+            for (int b : array) {
+                if (nums == b) {
+                    System.out.println("Found at element: " + vals.indexOf(nums));
+                }
+            }
+        } else System.out.println("That was not a valid input. Please try again.");
     }
 }

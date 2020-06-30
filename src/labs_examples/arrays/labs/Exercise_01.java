4f432b903b7eb5ff4bc44971493bb3db9ab59ca4
unknown <Journeyjava0@gmail.com>
2020-06-29 20:27:28 -0600

    uploading exercise 1

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  * Arrays calculator
  *
@@ -12,4 +14,30 @@ package labs_examples.arrays.labs;
 
 public class Exercise_01 {
 
+    public static void main(String[] args) {
+        //scanner and variable declaration
+        Scanner nums = new Scanner(System.in);
+        int[] input = new int[3];
+        int sum = 0;
+        int avg = 0;
+
+        //user instructions
+        System.out.print("The numbers, there will be 10 in total: ");
+
+        //populate the array with user's input
+        for(int i = 0; i < input.length; i++){
+        input[i] = nums.nextInt();
+        }
+
+        //calculate the sum
+        for (int j : input) {
+           sum += j;
+        }
+
+        //calculate average
+        avg = sum/input.length;
+
+        //Print out results
+        System.out.println("Total sum is: " + sum + " and the avarge is: " + avg);
+    }
 }

9a0b02801109be1c5ed6e9ef97eea6c9b167c47f
unknown <Journeyjava0@gmail.com>
2020-07-06 13:28:16 -0600

    final submission

--- src/labs_examples/objects_classes_methods/labs/objects/Crew.java
+++ src/labs_examples/objects_classes_methods/labs/objects/Crew.java
@@ -1,4 +1,39 @@
 package labs_examples.objects_classes_methods.labs.objects;
 
 public class Crew {
+//This is a POJO and is used in the Airplane class
+        int numPilots;
+        int numFlightAttendants;
+
+        public Crew(int numPilots, int numFlightAttendants) {
+            this.numPilots = numPilots;
+            this.numFlightAttendants = numFlightAttendants;
+        }
+
+        @Override
+        public String toString() {
+            return "Crew{" +
+                    "numPilots=" + numPilots +
+                    ", numFlightAttendants=" + numFlightAttendants +
+                    '}';
+        }
+
+        public int getNumPilots() {
+            return numPilots;
+        }
+
+        public void setNumPilots(int numPilots) {
+            this.numPilots = numPilots;
+        }
+
+        public int getNumFlightAttendants() {
+            return numFlightAttendants;
+        }
+
+        public void setNumFlightAttendants(int numFlightAttendants) {
+            this.numFlightAttendants = numFlightAttendants;
+        }
 }
+
+
+

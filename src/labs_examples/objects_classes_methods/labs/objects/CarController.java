4db08b67e9b423cbd67028fd96a75d5aa6e6d199
unknown <Journeyjava0@gmail.com>
2020-07-06 14:57:40 -0600

    final updates

--- src/labs_examples/objects_classes_methods/labs/objects/CarController.java
+++ src/labs_examples/objects_classes_methods/labs/objects/CarController.java
@@ -20,5 +20,9 @@ public class CarController {
         System.out.println("The third vehicle is an " + honda.getTransmission().type + " with " + honda.getTransmission().option);
         System.out.println("The fourth car is a " + lambo.getModel());
         System.out.println("The final car is a " + tesla.getYear() + " " + tesla.getModel() + " with a top speed of " + tesla.getSpeed() + "mph");
+
+        //just for fun, I'm going to create a jetEngine using the JetEngine POJO
+        JetEngine forFun = new JetEngine(4, "Lockheed Martin");
+        System.out.println("We also have a " + forFun.getEngineMake() + " jet engine, with " + forFun.getNumEngines() + " turbines");
     }
 }

9ff92f6ad259160d57b9657f8e1c946bd71a2c14
unknown <Journeyjava0@gmail.com>
2020-07-06 13:30:12 -0600

    final submission

--- src/labs_examples/objects_classes_methods/labs/objects/Seat.java
+++ src/labs_examples/objects_classes_methods/labs/objects/Seat.java
@@ -1,4 +1,38 @@
 package labs_examples.objects_classes_methods.labs.objects;
 
 public class Seat {
+//This is a POJO and is used in the Airplane class
+        int numSeats;
+        int numAisles;
+
+        public Seat(int numSeats, int numAisles) {
+            this.numSeats = numSeats;
+            this.numAisles = numAisles;
+        }
+
+        @Override
+        public String toString() {
+            return "Seats{" +
+                    "numSeats=" + numSeats +
+                    ", numAisles=" + numAisles +
+                    '}';
+        }
+
+        public int getNumSeats() {
+            return numSeats;
+        }
+
+        public void setNumSeats(int numSeats) {
+            this.numSeats = numSeats;
+        }
+
+        public int getNumAisles() {
+            return numAisles;
+        }
+
+        public void setNumAisles(int numAisles) {
+            this.numAisles = numAisles;
+        }
 }
+
+

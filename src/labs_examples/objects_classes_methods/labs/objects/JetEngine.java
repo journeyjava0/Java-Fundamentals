bc0cb700cc2d107a2e69b2c8e6eaab47ade8b08b
unknown <Journeyjava0@gmail.com>
2020-07-06 13:28:56 -0600

    final submission

--- src/labs_examples/objects_classes_methods/labs/objects/JetEngine.java
+++ src/labs_examples/objects_classes_methods/labs/objects/JetEngine.java
@@ -1,4 +1,40 @@
 package labs_examples.objects_classes_methods.labs.objects;
 
-public class Engine {
+public class JetEngine {
+//This is a POJO and is used in the Airplane class
+        int numEngines;
+        String engineMake;
+
+        public JetEngine(int numEngines, String engineMake) {
+            this.numEngines = numEngines;
+            this.engineMake = engineMake;
+        }
+
+        @Override
+        public String toString() {
+            return "Engines{" +
+                    "numEngines=" + numEngines +
+                    ", engineMake='" + engineMake + '\'' +
+                    '}';
+        }
+
+        public int getNumEngines() {
+            return numEngines;
+        }
+
+        public void setNumEngines(int numEngines) {
+            this.numEngines = numEngines;
+        }
+
+        public String getEngineMake() {
+            return engineMake;
+        }
+
+        public void setEngineMake(String engineMake) {
+            this.engineMake = engineMake;
+        }
 }
+
+
+
+

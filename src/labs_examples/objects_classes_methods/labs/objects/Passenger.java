27cfe3f0fa2edc265ea875786927cc1b6256c38e
unknown <Journeyjava0@gmail.com>
2020-07-06 13:29:40 -0600

    final submission

--- src/labs_examples/objects_classes_methods/labs/objects/Passenger.java
+++ src/labs_examples/objects_classes_methods/labs/objects/Passenger.java
@@ -1,4 +1,27 @@
 package labs_examples.objects_classes_methods.labs.objects;
 
 public class Passenger {
+//This is a POJO and is used in the Airplane class
+        int numPassengers;
+
+        public Passenger(int numPassengers) {
+            this.numPassengers = numPassengers;
+        }
+
+        @Override
+        public String toString() {
+            return "Passengers{" +
+                    "numPassengers=" + numPassengers +
+                    '}';
+        }
+
+        public int getNumPassengers() {
+            return numPassengers;
+        }
+
+        public void setNumPassengers(int numPassengers) {
+            this.numPassengers = numPassengers;
+        }
 }
+
+

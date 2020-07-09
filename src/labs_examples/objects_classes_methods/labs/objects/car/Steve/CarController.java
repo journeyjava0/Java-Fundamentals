f4ebf628dd451548e426eeb860e95e03ac47d6b5
unknown <Journeyjava0@gmail.com>
2020-07-09 17:01:36 -0600

    update

--- src/labs_examples/objects_classes_methods/labs/objects/car/Steve/CarController.java
+++ src/labs_examples/objects_classes_methods/labs/objects/car/Steve/CarController.java
@@ -1,4 +1,6 @@
-package labs_examples.objects_classes_methods.labs.objects;
+package labs_examples.objects_classes_methods.labs.objects.car.Steve;
+
+import labs_examples.objects_classes_methods.labs.objects.JetEngine;
 
 public class CarController {
     public static void main(String[] args) {
@@ -8,21 +10,25 @@ public class CarController {
 
         MyCar ford = new MyCar ("Ford", "red");
 
-        driveTrain theHonda = new driveTrain ("automatic", "AWD");
+        DriveTrain theHonda = new DriveTrain("automatic", "AWD");
         MyCar honda = new MyCar(true, theHonda);
 
-        fastCar lambo = new fastCar (500, "lambo");
+        FastCar lambo = new FastCar(500, "lambo");
 
-        electricCar tesla = new electricCar(250, "Tesla", 2019);
+        ElectricCar tesla = new ElectricCar(250, "Tesla", 2019);
 
         System.out.println("The first vehicle is a " + toyota.getYear() + " " + toyota.getType());
         System.out.println("The second vehicle is a " + ford.getColor() + " " + ford.getMake());
         System.out.println("The third vehicle is an " + honda.getTransmission().type + " with " + honda.getTransmission().option);
-        System.out.println("The fourth car is a " + lambo.getModel());
+        //System.out.println("The fourth car is a " + lambo.getModel());
         System.out.println("The final car is a " + tesla.getYear() + " " + tesla.getModel() + " with a top speed of " + tesla.getSpeed() + "mph");
 
         //just for fun, I'm going to create a jetEngine using the JetEngine POJO
         JetEngine forFun = new JetEngine(4, "Lockheed Martin");
         System.out.println("We also have a " + forFun.getEngineMake() + " jet engine, with " + forFun.getNumEngines() + " turbines");
+
+        FastCar lambo1 = new FastCar();
+        lambo1.setSpeed(100);
+        lambo1.setModel("A");
     }
 }

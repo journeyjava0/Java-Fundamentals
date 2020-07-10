aa938c282da81f5356c22953182e26233b3a3321
unknown <Journeyjava0@gmail.com>
2020-07-09 22:35:26 -0600

    update

--- src/labs_examples/objects_classes_methods/labs/objects/car/Steve/DriveTrain.java
+++ src/labs_examples/objects_classes_methods/labs/objects/car/Steve/DriveTrain.java
@@ -1,4 +1,37 @@
-package labs_examples.objects_classes_methods.labs.objects.car;
+package labs_examples.objects_classes_methods.labs.objects.car.Steve;
 
 public class DriveTrain {
+
+    String type;
+    String option;
+
+    public DriveTrain(String type, String option) {
+        this.type = type;
+        this.option = option;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+
+    @Override
+    public String toString() {
+        return "DriveTrain{" +
+                "type='" + type + '\'' +
+                ", option='" + option + '\'' +
+                '}';
+    }
+
 }

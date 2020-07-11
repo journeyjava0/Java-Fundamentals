1e787d2079d1b47909d031eed3eecff4e5b5fa3d
unknown <Journeyjava0@gmail.com>
2020-07-11 15:41:41 -0600

    update

--- src/labs_examples/objects_classes_methods/labs/objects/car/Steve/FastCar.java
+++ src/labs_examples/objects_classes_methods/labs/objects/car/Steve/FastCar.java
@@ -1,4 +1,41 @@
-package labs_examples.objects_classes_methods.labs.objects.car;
+package labs_examples.objects_classes_methods.labs.objects.car.Steve;
 
 public class FastCar {
+
+    private int speed;
+    private String model;
+
+    public FastCar(int speed, String model) {
+        this.speed = speed;
+        this.model = model;
+    }
+
+    public FastCar (){}
+
+    @Override
+    public String toString() {
+        return "FastCar{" +
+                "speed=" + speed +
+                ", model=" + model +
+                '}';
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        if (speed >= 0){
+            this.speed = speed;
+        }
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
 }
+

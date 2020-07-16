d71a7d52dc71c4ac2588ec3133845152e531ce7c
unknown <Journeyjava0@gmail.com>
2020-07-15 23:08:54 -0600

    update

--- src/labs_examples/objects_classes_methods/demo/BookController.java
+++ src/labs_examples/objects_classes_methods/demo/BookController.java
@@ -1,4 +1,25 @@
 package labs_examples.objects_classes_methods.demo;
 
+//import labs_examples.arrays.labs.Book;
+
+import labs_examples.arrays.labs.Book;
+
 public class BookController {
+
+    public static void main(String[] args) {
+        Book flyFishing = new Book();
+        flyFishing.setAuthor("Joseph Peterman");
+        flyFishing.setTitle("First Cast to First Fish");
+        flyFishing.setNumPages(80);
+
+        Book universe = new Book("Stars & Planets", "Desmond Tutu", 580);
+
+        Library set1 = new Library();
+        set1.books.add(flyFishing);
+        set1.books.add(universe);
+        for (Book book : set1.books){
+            System.out.println(book.toString());
+        }
+
+    }
 }

0588af2932b0f308594dd038993b88de63af1bb2
unknown <Journeyjava0@gmail.com>
2020-07-18 17:34:13 -0600

    update

--- src/labs_examples/objects_classes_methods/demo/Library.java
+++ src/labs_examples/objects_classes_methods/demo/Library.java
@@ -1,4 +1,9 @@
 package labs_examples.objects_classes_methods.demo;
 
+import labs_examples.arrays.labs.Book;
+
+import java.util.ArrayList;
+
 public class Library {
+    ArrayList<Book> books = new ArrayList<>();
 }

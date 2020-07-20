baa2efba115a57f83707728419acbe23153a6d78
unknown <Journeyjava0@gmail.com>
2020-07-19 19:20:38 -0600

    update

--- src/labs_examples/objects_classes_methods/labs/objects/AirplaneController.java
+++ src/labs_examples/objects_classes_methods/labs/objects/AirplaneController.java
@@ -23,7 +23,18 @@ public class AirplaneController {
         Airplane ua2 = new Airplane(ua2Engines, ua2Crew, ua2Seats, ua2Passengers);
 
         //Print out some values from the objects
+
         System.out.println("Flight 1 has " + ua1.getPassengers().numPassengers + " passengers " + "and flight 2 has " + ua2.getPassengers().getNumPassengers() + " passengers");
+
+    //This is the example from the methods labs
+        Crew newCrew1 = new Crew (5, 10); //instance variable (i.e. object) is set to private in the Airplane class
+        ua2.setCrews(newCrew1); //line 30 creates a new object 'newCrew1' and line 31 uses the setter method to change the value of the
+                                //arguments of the object 'newCrew1'
+        System.out.println("new crew is " + ua2.getCrews());
+        System.out.println(newCrew1);//I get the same result whether .toString is used or not on newCrew1 ... is that
+                                                //because the .toString method is already applied to the 'Crew' object so
+                                                //any instance of the Crew class when called will automatically be returned
+                                                //becuase anytime the 'Crew' object is called it returns a String? (line 14 of crew class).
     }
 }
 

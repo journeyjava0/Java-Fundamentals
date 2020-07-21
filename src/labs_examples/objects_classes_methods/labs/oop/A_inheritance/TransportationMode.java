3a727560c8896ac59576f28de9c23bf005a9a823
unknown <Journeyjava0@gmail.com>
2020-07-20 21:46:12 -0600

    uploading super to all

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/TransportationMode.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/TransportationMode.java
@@ -1,4 +1,47 @@
 package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 
 public class TransportationMode {
+    protected boolean gasEngine;
+    protected boolean electricEngine;
+    protected boolean noEngine;
+
+    public TransportationMode () {
+
+    }
+
+    public void startVehicle () {
+        System.out.println("Step 1: place key in ignition");
+    }
+
+    public TransportationMode(boolean gasEngine, boolean electricEngine, boolean noEngine) {
+        this.gasEngine = gasEngine;
+        this.electricEngine = electricEngine;
+        this.noEngine = noEngine;
+    }
+
+    public boolean isGasEngine() {
+        return gasEngine;
+    }
+
+    public void setGasEngine(boolean gasEngine) {
+        this.gasEngine = gasEngine;
+    }
+
+    public boolean isElectricEngine() {
+        return electricEngine;
+    }
+
+    public void setElectricEngine(boolean electricEngine) {
+        this.electricEngine = electricEngine;
+    }
+
+    public boolean isNoEngine() {
+        return noEngine;
+    }
+
+    public void setNoEngine(boolean noEngine) {
+        this.noEngine = noEngine;
+    }
+
+
 }

581d9a1b572e530c9b28bbea59cfed8edee6436c
unknown <Journeyjava0@gmail.com>
2020-07-20 21:45:16 -0600

    uploading super to Car

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/LandTransport.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/LandTransport.java
@@ -1,4 +1,32 @@
 package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 
-public class LandTransport {
+public class LandTransport extends TransportationMode {
+    protected int numWheels;
+    protected int numPax;
+
+    public LandTransport () {
+
+    }
+
+    public LandTransport(boolean gasEngine, boolean electricEngine, boolean noEngine, int numWheels, int numPax) {
+        super(gasEngine, electricEngine, noEngine);
+        this.numWheels = numWheels;
+        this.numPax = numPax;
+    }
+
+    public int getNumWheels() {
+        return numWheels;
+    }
+
+    public void setNumWheels(int numWheels) {
+        this.numWheels = numWheels;
+    }
+
+    public int getNumPax() {
+        return numPax;
+    }
+
+    public void setNumPax(int numPax) {
+        this.numPax = numPax;
+    }
 }

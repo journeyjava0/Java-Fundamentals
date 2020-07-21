c262d30f3e90029e086e4d1fc8910a365193bc3b
unknown <Journeyjava0@gmail.com>
2020-07-20 21:46:54 -0600

    uploading controller class

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/VehicleController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/VehicleController.java
@@ -1,4 +1,15 @@
 package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 
 public class VehicleController {
+
+    public static void main(String[] args) {
+        Car myCar = new Car(true, false, false, 4, 5, "Toyota", "Corolla");
+        myCar.startVehicle(); //override method in TransportationMode class
+
+        Car someCar = new Car ();
+        someCar.setMake("Ford");
+        someCar.setModel("Fiesta");
+        System.out.println(someCar.getMake() + " " + someCar.getModel());
+
+    }
 }

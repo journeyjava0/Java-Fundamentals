18d03609be448f58f0f8396e386f35a1db8fa764
unknown <Journeyjava0@gmail.com>
2020-07-25 10:55:13 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/RyanChallenge/Concrete.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/RyanChallenge/Concrete.java
@@ -1,4 +1,14 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.RyanChallenge;
 
-public class Concrete {
+public class Concrete implements Amount {
+
+    @Override
+    public void amnt(int num) {
+        System.out.println(num + " yards of concrete are needed");
+    }
+
+    @Override
+    public void cost(int cost) {
+        System.out.println("The total cost is $" + cost);
+    }
 }

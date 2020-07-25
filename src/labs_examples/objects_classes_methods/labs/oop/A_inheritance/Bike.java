5ce248d83203530c1d3e5cb076bc255dc2b5dd93
unknown <Journeyjava0@gmail.com>
2020-07-24 18:16:40 -0600

    update

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Bike.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Bike.java
@@ -1,4 +1,27 @@
 package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 
-public class Bike {
+public class Bike extends LandTransport {
+
+    public Bike() {
+    }
+
+    public Bike(boolean gasEngine, boolean electricEngine, boolean noEngine, int numWheels, int numPax) {
+        super(gasEngine, electricEngine, noEngine, numWheels, numPax);
+    }
+
+    @Override
+    public void startVehicle() {
+        System.out.println("Just use the pedals");
+    }
+
+    @Override
+    public String toString() {
+        return "Bike{" +
+                "numWheels=" + numWheels +
+                ", numPax=" + numPax +
+                ", gasEngine=" + gasEngine +
+                ", electricEngine=" + electricEngine +
+                ", noEngine=" + noEngine +
+                '}';
+    }
 }

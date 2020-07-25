3981f44aa73a4843ba704996b484bdea885ae454
unknown <Journeyjava0@gmail.com>
2020-07-25 10:54:19 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/RyanChallenge/CementDriveway.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/RyanChallenge/CementDriveway.java
@@ -1,4 +1,8 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.RyanChallenge;
 
-public class CementDriveway {
+public class CementDriveway extends Concrete {
+    @Override
+    public void amnt(int num) {
+        System.out.println(num + " yards of cement are needed for the driveway");
+    }
 }

db140ba8bb43ec89757eb7da2cbb224f425c9d9a
unknown <Journeyjava0@gmail.com>
2020-07-25 10:53:00 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/RyanChallenge/BuildingMaterials.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/RyanChallenge/BuildingMaterials.java
@@ -1,4 +1,13 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.RyanChallenge;
 
 public class BuildingMaterials {
+   Lumber lumber;
+   Concrete concrete;
+
+    public BuildingMaterials(Lumber lumber, Concrete concrete) {
+        this.lumber = lumber;
+        this.concrete = concrete;
+    }
 }
+
+

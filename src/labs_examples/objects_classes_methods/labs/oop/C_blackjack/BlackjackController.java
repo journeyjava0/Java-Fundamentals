6d3bc6ca3fa331696f8a197add1e3e289692b4c2
unknown <Journeyjava0@gmail.com>
2020-07-27 20:09:20 -0600

    initial update

---
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/BlackjackController.java
@@ -0,0 +1,10 @@
+package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
+
+public class BlackjackController {
+
+    public static void main(String[] args) {
+        Deck a = new Deck();
+        a.createDeck();
+
+       }
+}

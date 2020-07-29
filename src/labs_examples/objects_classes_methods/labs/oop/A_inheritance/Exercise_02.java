1c7f266b7cdd5ba3733b48c427a440b125035a4a
unknown <Journeyjava0@gmail.com>
2020-07-28 22:25:58 -0600

    update

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
@@ -3,7 +3,9 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 /**
  * Run the class below and study the output. What prints and why?
  *
- * Your answer:
+ * Your answer: My answer was 20, which is wrong. I now know that because B extends A, B takes a copy of the
+ * variables from A which includes int i and even though an object a was created and set
+ * equal to a new object B, B still extends A, which means i = 10.
  *
  */
 class A {

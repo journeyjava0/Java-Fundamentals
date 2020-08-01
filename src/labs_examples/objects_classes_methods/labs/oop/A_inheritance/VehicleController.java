a1740aac7c40fd7e8d3628357d56aac417fd5f1b
unknown <Journeyjava0@gmail.com>
2020-07-31 18:25:49 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/VehicleController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/VehicleController.java
@@ -5,6 +5,7 @@ public class VehicleController {
     public static void main(String[] args) {
         Car myCar = new Car(true, false, false, 4, 5, "Toyota", "Corolla");
         myCar.startVehicle(); //override method in TransportationMode class
+        myCar.mpg(25); //override method in TransportationMode class
 
         Car someCar = new Car ();
         someCar.setMake("Ford");
@@ -14,9 +15,17 @@ public class VehicleController {
         TransportationMode test = new TransportationMode();
         test.startVehicle(); //calls method in Transportation Mode Class
 
-        //line 7 and 15 appear to be calls to the same method, however line 7 is overriding
-        //the method in line 15 which is polymorphism and line 15 is a call to the 'original' method.
+        Bike bicycle = new Bike(false, false, true, 2, 1);
+        bicycle.startVehicle();
 
+        //line 7 and 16 and 19 appear to be calls to the same method, however line 7 is overriding
+        //the method in line 16 which is polymorphism and line 16is a call to the 'original' method.
+        //line 19 is overriding the methods in the TransportationMode class
 
+        SportsCar lambo = new SportsCar();
+        lambo.setSpeed(500);
+        lambo.setMake("Lamborghini");
+        lambo.startVehicle();
+        System.out.println("Lambo speed is " + lambo.getSpeed() + " model is " + lambo.getMake());
     }
 }

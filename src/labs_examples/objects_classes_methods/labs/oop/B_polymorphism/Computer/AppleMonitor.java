c921c43266fd996a624eea89a61e8ef6eaa87f49
unknown <Journeyjava0@gmail.com>
2020-08-01 19:05:33 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Computer/AppleMonitor.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Computer/AppleMonitor.java
@@ -1,4 +1,8 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.Computer;
 
-public class AppleMonitor {
+public class AppleMonitor implements Monitor {
+    @Override
+    public int getResolution() {
+        return 1080;
+    }
 }

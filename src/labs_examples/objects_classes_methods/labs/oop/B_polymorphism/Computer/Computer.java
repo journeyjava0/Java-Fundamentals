938d4bcde0ca93695b94ce525b449d5da308c7ee
unknown <Journeyjava0@gmail.com>
2020-08-02 18:02:58 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Computer/Computer.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Computer/Computer.java
@@ -1,4 +1,13 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.Computer;
 
 public class Computer {
+   Monitor monitor;
+   CPU cpu;
+   HardDrive hd;
+
+    public Computer(Monitor monitor, CPU cpu, HardDrive hd) {
+        this.monitor = monitor;
+        this.cpu = cpu;
+        this.hd = hd;
+    }
 }

b1a8adf50def63e4d502e23a17c9faea0337fa59
unknown <Journeyjava0@gmail.com>
2020-08-03 20:23:25 -0600

    update

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Computer/DellMonitor.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Computer/DellMonitor.java
@@ -1,4 +1,8 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.Computer;
 
-public class DellMonitor {
+public class DellMonitor implements Monitor {
+    @Override
+    public int getResolution() {
+        return 4000;
+    }
 }

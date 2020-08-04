ed5e37ceeec5bb555918009a668d85271a52c412
unknown <Journeyjava0@gmail.com>
2020-08-04 16:14:28 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Computer/IntelCPU.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Computer/IntelCPU.java
@@ -1,4 +1,18 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.Computer;
 
-public class IntelCPU {
+public class IntelCPU extends CPU {
+    @Override
+    public int getClockSpeed() {
+        return 25000;
+    }
+
+    @Override
+    public void turnOnFan() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("intel cpu \n");
+        sb.append("overheating...");
+        sb.append("power on super duper fan");
+        sb.append("price: ").append(price);
+        System.out.println(sb.toString());
+    }
 }

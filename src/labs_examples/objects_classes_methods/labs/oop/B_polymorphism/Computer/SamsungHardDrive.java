b8d9673148aad391e88a6c2583c6e66c9178905c
unknown <Journeyjava0@gmail.com>
2020-08-06 19:29:57 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Computer/SamsungHardDrive.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Computer/SamsungHardDrive.java
@@ -1,4 +1,8 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.Computer;
 
-public class SamsungHardDrive {
+public class SamsungHardDrive implements HardDrive {
+    @Override
+    public int getCapacity() {
+        return 500;
+    }
 }

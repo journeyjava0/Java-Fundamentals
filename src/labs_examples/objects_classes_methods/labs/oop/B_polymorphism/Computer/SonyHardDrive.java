7e206c6e4cb343b4b158c5739f8a64caa6a491b4
unknown <Journeyjava0@gmail.com>
2020-08-07 20:55:50 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Computer/SonyHardDrive.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Computer/SonyHardDrive.java
@@ -1,4 +1,8 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.Computer;
 
-public class SonyHardDrive {
+public class SonyHardDrive implements HardDrive {
+    @Override
+    public int getCapacity() {
+        return 1000;
+    }
 }

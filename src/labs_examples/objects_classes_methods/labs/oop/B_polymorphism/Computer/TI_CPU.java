5c161a7a33ef26af5b1d6ac2e154bca4b8e63c6a
unknown <Journeyjava0@gmail.com>
2020-08-08 23:32:32 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Computer/TI_CPU.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/Computer/TI_CPU.java
@@ -1,4 +1,18 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.Computer;
 
-public class TI_CPU {
+public class TI_CPU extends CPU {
+    @Override
+    public int getClockSpeed() {
+        return 5000;
+    }
+
+    @Override
+    public void turnOnFan() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("TI cpu \n");
+        sb.append("screaming loudly...");
+        sb.append("where's my fan?!");
+        System.out.println(sb.toString());
+    }
+
 }

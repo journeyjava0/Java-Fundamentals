0a49708474558c05fab9831204f88b735d5ca5d5
unknown <Journeyjava0@gmail.com>
2020-08-11 23:54:01 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/examplePoly1/CarConvention.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/examplePoly1/CarConvention.java
@@ -1,4 +1,13 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.examplePoly1;
 
 public class CarConvention {
+    private CarDealerIntFace dealer;
+
+    public CarConvention(CarDealerIntFace dealer) {
+        this.dealer = dealer;
+    }
+
+    public void info () {
+        dealer.inventory();
+    }
 }

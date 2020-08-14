7f355ccf137ca1e3b693d747800004785cf046d6
unknown <Journeyjava0@gmail.com>
2020-08-13 23:22:21 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/examplePoly1/CarDealer1.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/examplePoly1/CarDealer1.java
@@ -1,4 +1,19 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.examplePoly1;
 
-public class CarDealer1 {
+public class CarDealer1 implements CarDealerIntFace {
+    @Override
+    public void salePrice(int salePrice, int discounts, double tax) {
+        System.out.println("Price is $" + (salePrice - discounts * tax));
+    }
+
+    @Override
+    public void tradeIn(int tradeValue, int markdown) {
+        System.out.println("Tradein value is $" + (tradeValue - markdown));
+    }
+
+    @Override
+    public void inventory() {
+        String greeting = "There are vehicles on this lot";
+        System.out.println(greeting);
+    }
 }

67bc46f77ed08a6690a80227aa28bc35aca0ee29
unknown <Journeyjava0@gmail.com>
2020-08-15 16:56:13 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/examplePoly1/CarDealerIntFace.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/examplePoly1/CarDealerIntFace.java
@@ -1,4 +1,7 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.examplePoly1;
 
-public class CarDealerIntFace {
+public interface CarDealerIntFace {
+    public void salePrice(int salePrice, int discounts, double tax);
+    public void tradeIn(int tradeValue, int markdown);
+    public void inventory ();
 }

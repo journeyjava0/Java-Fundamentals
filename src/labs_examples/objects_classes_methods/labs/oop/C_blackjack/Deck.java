b37d8cf77f82e7a8f6bc0b3f8ac50148edb34313
unknown <Journeyjava0@gmail.com>
2020-08-16 17:49:15 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Deck.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Deck.java
@@ -3,7 +3,6 @@ package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Deck {
 
@@ -17,10 +16,10 @@ public class Deck {
         populateDeck();
     }
 
-    public void populateDeck () {
+    public void populateDeck() {
         int index = 0;
-        for(int i = 1; i < 14; i++){
-            for(String s : suits) {
+        for (int i = 1; i < 14; i++) {
+            for (String s : suits) {
                 Card c = new Card(i, s);
                 cards[index] = c;
                 index++;
@@ -28,11 +27,11 @@ public class Deck {
         }
     }
 
-    public void deal(Player player){
+    public void deal(Player player) {
         Random r = new Random();
         int index = r.nextInt(52);
 
-        while (usedCards.contains(index)){
+        while (usedCards.contains(index)) {
             index = r.nextInt(52);
         }
 
@@ -41,59 +40,62 @@ public class Deck {
         player.getHand().getCards().add(c);
     }
 
-     public void dealAnotherCard (Player p1, Player p2) {
-         Scanner anotherCard = new Scanner(System.in);
-         System.out.println(p1.getName() + " do you want another card? (y/n)");
-         String hit = anotherCard.next();
+//Didn't use this method, but keeping it here just in case
+//    public void computerAI(Player p2, Player p1){
+//        if (p2.getHand().getHandValue() <= 16 || p2.getHand().getHandValue() < p1.getHand().getHandValue()) {
+//            System.out.println("Computer has " + p2.getHand() + " and will hit");
+//            deal(p2);
+//            System.out.println("Computer has " + p2.getHand());
+//        } else if (p2.getHand().getHandValue() > 21) {
+//            System.out.println("Computer lost");
+//        }
+//    }
 
-         if (hit.equalsIgnoreCase("n")) {
-             System.out.println(p1.getName() + " Your cards are " + p1.getHand());
-
-             if (p2.getHand().getHandValue() <= 16 || p2.getHand().getHandValue() < p1.getHand().getHandValue()){
-                 System.out.println("Computer has " + p2.getHand() + " and will hit");
-                 deal(p2);
-                 System.out.println("Computer has " + p2.getHand());
-             }
-
-             if (p2.getHand().getHandValue() > 21) {
-                 System.out.println("Computer lost");
-                 return;
-             }
-         }
-
-         if (hit.equalsIgnoreCase("y")) {
-             deal(p1);
-             System.out.println(p1.getName() + " Your cards are " + p1.getHand());
-
-             if (p2.getHand().getHandValue() < 16) {
-                 System.out.println("Computer has " + p2.getHand() + " and will hit");
-                 deal(p2);
-                 System.out.println("Computer has " + p2.getHand());
-                    if (p2.getHand().getHandValue() > 21) {
-                        System.out.println("Computer lost");
-                        return;
-                    }
-                     } else System.out.println("Computer stays " + p2.getHand());
-
-
-                     if (p1.getHand().getHandValue() < 21) {
-                         dealAnotherCard(p1, p2);
-                     }
-
-                     if (p1.getHand().getHandValue() == 21 || p2.getHand().getHandValue() == 21) {
-                         return;
-                     }
-
-                     if (p1.getHand().getHandValue() > 21 || p1.getHand().getHandValue() > 21) {
-                         return;
-                     }
-                 }
-
-         if (!hit.equalsIgnoreCase("y") && !hit.equalsIgnoreCase("n")) {
-                     System.out.println("not a valid input, try again");
-                     dealAnotherCard(p1, p2);
-                 }
-             }
+//Didn't actually use this method, but keeping it here just in case
+//    public void dealAnotherCard(Player p1, Player p2) {
+//        Scanner anotherCard = new Scanner(System.in);
+//        System.out.println(p1.getName() + " do you want another card? (y/n)");
+//        String hit = anotherCard.next();
+//
+//        if (hit.equalsIgnoreCase("n")) {
+//            System.out.println(p1.getName() + " Your cards are " + p1.getHand());
+//        }
+//
+//        computerAI(p1, p2);
+//
+//        if (hit.equalsIgnoreCase("y")) {
+//            deal(p1);
+//            System.out.println(p1.getName() + " Your cards are " + p1.getHand());
+//
+//            if (p2.getHand().getHandValue() < 16) {
+//                System.out.println("Computer has " + p2.getHand() + " and will hit");
+//                deal(p2);
+//                System.out.println("Computer has " + p2.getHand());
+//                if (p2.getHand().getHandValue() > 21) {
+//                    System.out.println("Computer lost");
+//                    return;
+//                }
+//            } else System.out.println("Computer stays " + p2.getHand());
+//
+//
+//            if (p1.getHand().getHandValue() < 21) {
+//                dealAnotherCard(p1, p2);
+//            }
+//
+//            if (p1.getHand().getHandValue() == 21 || p2.getHand().getHandValue() == 21) {
+//                return;
+//            }
+//
+//            if (p1.getHand().getHandValue() > 21 || p1.getHand().getHandValue() > 21) {
+//                return;
+//            }
+//        }
+//
+//        if (!hit.equalsIgnoreCase("y") && !hit.equalsIgnoreCase("n")) {
+//            System.out.println("not a valid input, try again");
+//            dealAnotherCard(p1, p2);
+//        }
+//    }
 
     @Override
     public String toString() {

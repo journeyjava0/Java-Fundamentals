9727a8bb16cb6bd0eac21bb557027be8aa42b907
unknown <Journeyjava0@gmail.com>
2020-08-16 13:54:15 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/examplePoly1/Dealer.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/examplePoly1/Dealer.java
@@ -1,4 +1,44 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.examplePoly1;
 
-public class LocalDealer {
+public abstract class Dealer implements CarDealerIntFace{
+    String dealerName;
+    String country;
+
+    public Dealer () {
+
+    }
+
+    public Dealer(String dealerName, String country) {
+        this.dealerName = dealerName;
+        this.country = country;
+    }
+
+    public String getDealerName() {
+        return dealerName;
+    }
+
+    public void setDealerName(String dealerName) {
+        this.dealerName = dealerName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Dealer{" +
+                "dealerName='" + dealerName + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+
+    @Override
+    public void tradeIn(int tradeValue, int markdown) {
+        System.out.println("Dealer trade in");
+    }
 }

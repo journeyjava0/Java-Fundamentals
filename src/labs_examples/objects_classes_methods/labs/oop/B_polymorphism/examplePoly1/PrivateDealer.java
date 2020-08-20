fe3ec74f0ec672b31c1014e66d144783acb99d1b
unknown <Journeyjava0@gmail.com>
2020-08-19 21:17:14 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/examplePoly1/PrivateDealer.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/examplePoly1/PrivateDealer.java
@@ -1,4 +1,14 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.examplePoly1;
 
-public class PrivateDealer {
+public class PrivateDealer extends StateDealer {
+
+    @Override
+    public void salePrice(int salePrice, int discounts, double tax) {
+        System.out.println("private dealer sale price");
+    }
+
+    @Override
+    public void inventory() {
+        System.out.println("private delaer inventory");
+    }
 }

d03e2d92e71184a9a51e23619ba05232d5bf078d
unknown <Journeyjava0@gmail.com>
2020-08-21 18:31:03 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/examplePoly1/StateDealer.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/examplePoly1/StateDealer.java
@@ -1,4 +1,35 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.examplePoly1;
 
-public class StateDealer {
+public abstract class StateDealer extends Dealer {
+    String state;
+
+    public StateDealer () {
+
+    }
+
+    public StateDealer(String dealerName, String country, String state) {
+        super(dealerName, country);
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void greeting (String dealerName) {
+        System.out.println("You are currently browsing the " + dealerName);
+    }
+
+    @Override
+    public String toString() {
+        return "StateDealer{" +
+                "state='" + state + '\'' +
+                ", dealerName='" + dealerName + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
 }

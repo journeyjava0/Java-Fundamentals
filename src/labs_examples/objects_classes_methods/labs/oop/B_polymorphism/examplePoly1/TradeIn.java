8b4bdc9671560aa422236d593e3cd64701d15e20
unknown <Journeyjava0@gmail.com>
2020-08-22 19:22:43 -0600

    upload

--- src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/examplePoly1/TradeIn.java
+++ src/labs_examples/objects_classes_methods/labs/oop/B_polymorphism/examplePoly1/TradeIn.java
@@ -1,4 +1,8 @@
 package labs_examples.objects_classes_methods.labs.oop.B_polymorphism.examplePoly1;
 
-public class TradeIn {
+abstract class TradeIn implements CarDealerIntFace {
+    @Override
+    public void tradeIn(int tradeValue, int markdown) {
+        System.out.println("Your trade in is " + (tradeValue - markdown));
+    }
 }

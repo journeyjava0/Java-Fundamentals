5ad2959d8f5e46e02c56a33ebf4b62840fdddce2
unknown <Journeyjava0@gmail.com>
2020-09-05 20:22:44 -0600

    upload

--- src/labs_examples/static_nonstatic/labs/Methods.java
+++ src/labs_examples/static_nonstatic/labs/Methods.java
@@ -1,2 +1,18 @@
-package labs_examples.static_nonstatic.labs;public class Methods {
+package labs_examples.static_nonstatic.labs;
+
+public class Methods {
+
+    public static void greeting3() {
+        System.out.println("Hello, this is a static method from another Class");
+    }
+
+    public void greeting4() {
+        System.out.println("Hello, this is a non static method from another Class");
+    }
+
+    public void greeting5(){
+        System.out.println("Hello, I'm a non-static method in another class");
+    }
+
 }
+

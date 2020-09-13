fcda9274d0747dc20341d65e0b4e878fe96221af
unknown <Journeyjava0@gmail.com>
2020-09-12 22:13:23 -0600

    updates

--- src/labs_examples/exception_handling/labs/Exercise_03.java
+++ src/labs_examples/exception_handling/labs/Exercise_03.java
@@ -6,3 +6,17 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a try/catch/finally block.
  *
  */
+
+public class Exercise_03 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,4,0};
+
+        try{
+            System.out.println(nums[2] / nums[3]);
+        } catch (ArithmeticException exc){
+            System.out.println("Error, cannot divide by 0");
+        } finally {
+            System.out.println("All done :)");
+        }
+    }
+}
--- src/labs_examples/exception_handling/labs/Exercise_04.java
+++ src/labs_examples/exception_handling/labs/Exercise_04.java
@@ -6,3 +6,19 @@ package labs_examples.exception_handling.labs;
  *      Demonstrate a nested try/catch.
  *
  */
+public class Exercise_04 {
+    public static void main(String[] args) {
+        int nums[] = {1,2,4,0};
+
+        //Is this actually a nested try/catch?
+        try {
+            System.out.println(nums[2]);
+        } catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("Outside array bounds");
+        } try {
+            System.out.println(nums[1]/nums[3]);
+        } catch (ArithmeticException exc2){
+            System.out.println("Error, dividing by 0");
+        }
+    }
+}

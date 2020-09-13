fde4a6bee2633904235339a7ab07331662b21bbb
unknown <Journeyjava0@gmail.com>
2020-09-12 21:40:15 -0600

    upload

--- src/labs_examples/exception_handling/labs/EndOfBookException.java
+++ src/labs_examples/exception_handling/labs/EndOfBookException.java
@@ -1,2 +1,7 @@
-package labs_examples.exception_handling.labs;public class EndOfBookException {
+package labs_examples.exception_handling.labs;
+
+public class EndOfBookException extends Exception {
+    public EndOfBookException(String message) {
+        super(message);
+    }
 }

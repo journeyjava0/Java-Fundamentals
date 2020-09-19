ab0f76acc3bd995dc850fe62f922236ee697f896
unknown <Journeyjava0@gmail.com>
2020-09-18 18:18:16 -0600

    upload

--- src/labs_examples/input_output/labs/Exercise_01.java
+++ src/labs_examples/input_output/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.input_output.labs;
 
+import java.io.*;
+
 /**
  * Input/Output Exercise 1: File input/output
  *
@@ -9,10 +11,32 @@ package labs_examples.input_output.labs;
  *
  */
 
-class Example {
+class Exercise_01 {
     public static void main(String[] args) {
+        System.out.println(new File(".").getAbsoluteFile());
+        //Question: How do I specify the file path?
+        File relativeFile = new File("C:\\Users\\Owner\\Desktop\\Coding\\Learning_Java\\CodingNomads\\labs\\online-java-fundamentals\\src\\labs_examples\\input_output\\labs\\OriginalFile.txt");
+        //"C:\Users\Owner\Desktop\Coding\Learning Java\CodingNomads\labs\online-java-fundamentals\src\labs_examples\input_output\labs\OriginalFile.txt"
+        //"/labs/OriginalFile.txt"
+        File outputFilePath = new File("C:\\Users\\Owner\\Desktop\\OutputFile.txt");
 
 
+        try(FileReader input2 = new FileReader(relativeFile);
+            BufferedReader bufferedReader2 = new BufferedReader(input2);
+            FileWriter fileWriter2 = new FileWriter(outputFilePath)) {
+
+
+            int bytesRead = 0;
+            while ((bytesRead = bufferedReader2.read()) != -1){
+                fileWriter2.write(bytesRead);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
+

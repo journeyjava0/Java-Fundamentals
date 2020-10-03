d4fdab32bdf215a2d8ee2e40c823e263eded1090
unknown <Journeyjava0@gmail.com>
2020-10-02 21:44:26 -0600

    uploading Exercise 01

--- src/labs_examples/multi_threading/labs/Excercise_01.java
+++ src/labs_examples/multi_threading/labs/Excercise_01.java
@@ -13,8 +13,8 @@ class Exercise_01 {
 
     public static void main(String[] args) {
         System.out.println("Main thread Started");
-        MyFirstRunnable thread1 = new MyFirstRunnable ("First Thread Ever!");
-        MyFirstRunnable thread2 = new MyFirstRunnable ("Second thread");
+        MyFirstRunnable thread1 = new MyFirstRunnable("First Thread Ever!");
+        MyFirstRunnable thread2 = new MyFirstRunnable("Second thread");
         MyFirstRunnable thread3 = new MyFirstRunnable("Third thread");
 
         Thread t4 = new Thread(new MyRunnable1());
@@ -23,9 +23,9 @@ class Exercise_01 {
 
         System.out.println("Main thread ended");
     }
-
+}
     //Question for Ryan: Why does this need to be static?
-    public class MyFirstRunnable implements Runnable {
+    class MyFirstRunnable implements Runnable {
 
         //Instantiate Thread class
         Thread thread;
@@ -35,7 +35,6 @@ class Exercise_01 {
             thread = new Thread(this, name);
             thread.start();
         }
-
 
         //Overriding the Runnable Method. This is what is executed when the thread starts.
         @Override
@@ -52,4 +51,4 @@ class Exercise_01 {
             System.out.println(thread.getName() + " is terminating");
         }
     }
-}
+

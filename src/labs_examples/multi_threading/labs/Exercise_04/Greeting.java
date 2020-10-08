48b9029c3efd7f8ea991d36cc4d2dbff739300d2
Owner <journeyjava0@gmail.com>
2020-10-07 19:33:16 -0600

    uploading Exercise 4

--- src/labs_examples/multi_threading/labs/Exercise_04/Greeting.java
+++ src/labs_examples/multi_threading/labs/Exercise_04/Greeting.java
@@ -1,2 +1,14 @@
-package labs_examples.multi_threading.labs;public class Greeting {
+package labs_examples.multi_threading.labs.Exercise_04;
+
+//A class for a Greeting with a method that displays a simple message
+public class Greeting {
+    public synchronized void sayMessage (String msg){
+        System.out.println("The message is " + msg);
+        try{
+            Thread.sleep(1000);
+        } catch (Exception e){
+            System.out.println("Thread interrupted");
+        }
+        System.out.println("Message was displayed was " + msg);
+    }
 }

eaf791d92a219e94cd891b53ab28700f49396d32
Owner <journeyjava0@gmail.com>
2020-10-11 19:18:43 -0600

    upload

--- src/labs_examples/multi_threading/labs/Exercise_06/Exercise_06.java
+++ src/labs_examples/multi_threading/labs/Exercise_06/Exercise_06.java
@@ -1,4 +1,4 @@
-package labs_examples.multi_threading.labs;
+package labs_examples.multi_threading.labs.Exercise_06;
 
 /**
  * Multithreading Exercise 6:
@@ -8,14 +8,5 @@ package labs_examples.multi_threading.labs;
  */
 
 
-//thread 1 prints i / 2 %0 (evens)
-//wait until thread 2 is done
-//notify thread 2 that thread 1 is done
 
 
-//thread 2 prints i / 2 %1 of 1 to 100 (odds)
-//wait until thread 1 is done
-//notify thread 1 that thread 2 is done
-
-
-//or just use 'synchronized' on the object that prints out the numbers?

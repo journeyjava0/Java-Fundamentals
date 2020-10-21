a884353134d7713453a0addf713f34b05a32aa85
Owner <journeyjava0@gmail.com>
2020-10-21 14:19:25 -0600

    final lambda update

--- src/labs_examples/lambdas/labs/Exercise_03.java
+++ src/labs_examples/lambdas/labs/Exercise_03.java
@@ -1,5 +1,11 @@
 package labs_examples.lambdas.labs;
 
+import labs_examples.packages.labs.Vehicles.Car;
+
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 /**
  * Lambdas Exercise 3:
  *
@@ -8,3 +14,20 @@ package labs_examples.lambdas.labs;
  *      3) Demonstrate the use of a constructor reference
  *
  */
+public class Exercise_03{
+    public static void main(String[] args) {
+        //static method reference
+        Consumer<Integer> whatSpeed = CarPOJO::setSpeed;
+        CarPOJO.setSpeed(50);
+
+        //instance method reference
+        Consumer<CarPOJO> carStatus = CarPOJO::stopCar;
+        CarPOJO myCar2 = new CarPOJO("Kia", "red");
+        carStatus.accept(myCar2);
+
+        //Constructor reference
+        BiFunction<String, String, CarPOJO> myCar = CarPOJO::new;
+        System.out.println(myCar.apply("Ford", "blue"));
+        System.out.println(myCar.apply("GMC", "black"));
+        }
+}

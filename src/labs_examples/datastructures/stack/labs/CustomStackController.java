8ceeafa283a092d027edb3f0b56275eea4cb2f87
Owner <journeyjava0@gmail.com>
2020-11-18 00:14:55 -0700

    uploading the custom stack controller

--- src/labs_examples/datastructures/stack/labs/CustomStackController.java
+++ src/labs_examples/datastructures/stack/labs/CustomStackController.java
@@ -1,2 +1,18 @@
-package labs_examples.datastructures.stack.labs;public class CustomStackController {
+package labs_examples.datastructures.stack.labs;
+
+public class CustomStackController {
+    public static void main(String[] args) {
+    CustomStackArray<String> myStack = new CustomStackArray<>();
+        myStack.push("cat");
+        myStack.push("dog");
+        myStack.push("bird");
+        myStack.push("car");
+
+
+        System.out.println("First element is: " + myStack.peekFirst() + "\n"); // cat
+        myStack.pop(); // remove car
+        System.out.println("Last element is: " + myStack.peekLast() + "\n"); // bird
+        System.out.println("All items remaining are: ");
+        myStack.printAll(); //cat, dog, bird
+    }
 }

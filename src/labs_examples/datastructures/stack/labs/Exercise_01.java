dbff8db7a0bf86010ce59e52ca92215e7531e5e4
Owner <journeyjava0@gmail.com>
2020-11-17 22:32:56 -0700

    uploading exercise 1 for stacks

--- src/labs_examples/datastructures/stack/labs/Exercise_01.java
+++ src/labs_examples/datastructures/stack/labs/Exercise_01.java
@@ -1,8 +1,24 @@
 package labs_examples.datastructures.stack.labs;
 
+import java.util.Stack;
+
 /**
  *      Demonstrate your mastery of Java's built-in Stack class by completing the following:
  *
  *      1) instantiate a new Stack
  *      2) demonstrate the use of EVERY method in the Stack class - there are 5
  */
+
+public class Exercise_01 {
+
+    public static void main(String[] args) {
+    Stack<String> myStack = new Stack<>();
+    myStack.push("cat");
+    myStack.push("dog");
+    myStack.pop();
+    System.out.println(myStack.peek());
+    myStack.empty();
+    myStack.search(1);
+
+    }
+}

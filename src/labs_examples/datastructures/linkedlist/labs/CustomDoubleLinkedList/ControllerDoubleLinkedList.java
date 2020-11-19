81e274900c0cf7710dbe222c39cd981c5bf34180
Owner <journeyjava0@gmail.com>
2020-11-18 21:18:05 -0700

    uploading double linked list

--- src/labs_examples/datastructures/linkedlist/labs/CustomDoubleLinkedList/ControllerDoubleLinkedList.java
+++ src/labs_examples/datastructures/linkedlist/labs/CustomDoubleLinkedList/ControllerDoubleLinkedList.java
@@ -4,17 +4,24 @@ package labs_examples.datastructures.linkedlist.labs.CustomDoubleLinkedList;
 
 public class ControllerDoubleLinkedList {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         MyDoubleLinkedList<String> myDoubleList = new MyDoubleLinkedList();
-        myDoubleList.addToFront("One");
-        myDoubleList.addToFront("Two");
-        myDoubleList.addToFront("Three");
-        myDoubleList.addToFront("Four");
+        myDoubleList.addToEnd("One");
+        myDoubleList.addToEnd("Two");
+        myDoubleList.addToEnd("Three");
+        myDoubleList.addToEnd("Four");
         myDoubleList.printList(); //expecting Four, Three, Two, One
         myDoubleList.addToEnd("Five");
         myDoubleList.printList(); //expecting Four, Three, Two, One, Five
-        myDoubleList.remove("Five");
-        myDoubleList.printList();
-        myDoubleList.remove("Two");
-        myDoubleList.printList();//expecting four, three, one
+        myDoubleList.printNeighbours("One");
+        myDoubleList.printNeighbours("Three");
+        myDoubleList.printNeighbours("Five");
+//        myDoubleList.remove("Five");
+//        myDoubleList.printList();
+//        myDoubleList.remove("Two");
+//        myDoubleList.printList();//expecting four, three, one
+//        long endTime = System.currentTimeMillis();
+//        long diff = endTime - startTime;
+//        System.out.println(diff);
     }
 }

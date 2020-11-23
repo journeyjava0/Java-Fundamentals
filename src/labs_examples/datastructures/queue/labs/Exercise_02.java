69dd52f7e186b9b0480495a493648efd4f170a93
Owner <journeyjava0@gmail.com>
2020-11-23 15:19:01 -0700

    upload queue ex 2

--- src/labs_examples/datastructures/queue/labs/Exercise_02.java
+++ src/labs_examples/datastructures/queue/labs/Exercise_02.java
@@ -1,18 +1,19 @@
 package labs_examples.datastructures.queue.labs;
 
-/**
- *      Queues - Exercise_02
- *
- *      Write a new custom (Generic) Queue class that uses a traditional Array as the underlying data structure
- *      rather than the LinkedList as in the example we have already seen.
- *
- *      Your custom Queue class must also do the following:
- *
- *      1) throw a custom exception when trying to pop  an element from an empty Queue
- *      2) resize the Queue (the underlying array) to be twice the size when the Queue is more than 3/4 full
- *      3) resize the Queue (the underlying array) to be half the size when the Queue is more than 1/4 empty
- *      4) contain the methods peekFirst() and peekLast()
- *      5) contain a size() method
- *      6) contain a method to print out the data of all elements in the Queue
- *
- */
+public class Exercise_02 {
+    public static void main(String[] args) {
+        CustomQueue<String> myCustomQueue = new CustomQueue<>();
+        myCustomQueue.add("Birch");
+        myCustomQueue.add("Cedar");
+        myCustomQueue.add("Spruce");
+        myCustomQueue.add("Oak");
+        myCustomQueue.printAll();
+        System.out.println(" ");
+
+        myCustomQueue.clearAll();
+        System.out.println(" ");
+
+        System.out.println(myCustomQueue.getArraySize());
+        myCustomQueue.printAll();
+    }
+}

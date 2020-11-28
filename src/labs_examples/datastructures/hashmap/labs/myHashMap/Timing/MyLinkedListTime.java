9ff1a1c9fa0958397a366b4033ea667fc0db2c6e
Owner <journeyjava0@gmail.com>
2020-11-27 20:22:40 -0700

    uploading hashmap exercise 3

--- src/labs_examples/datastructures/hashmap/labs/myHashMap/Timing/MyLinkedListTime.java
+++ src/labs_examples/datastructures/hashmap/labs/myHashMap/Timing/MyLinkedListTime.java
@@ -1,2 +1,27 @@
-package labs_examples.datastructures.hashmap.labs.myHashMap.Timing;public class MyLinkedListTime {
+package labs_examples.datastructures.hashmap.labs.myHashMap.Timing;
+
+import java.util.LinkedList;
+
+public class MyLinkedListTime {
+    public static void runTimeLinkedList() {
+        LinkedList<Integer> myLinkedList = new LinkedList<>();
+        Timer timeLinkedList = new Timer("Linked List");
+
+        timeLinkedList.startTimer();
+        for (int i = 0; i < 100000; i++) {
+            myLinkedList.add(i);
+        }
+        timeLinkedList.endTimer(timeLinkedList.getStart());
+
+        timeLinkedList.startTimer();
+        if(myLinkedList.contains(9999)){
+            timeLinkedList.endTimer(timeLinkedList.getStart());
+        }
+
+        timeLinkedList.startTimer();
+        myLinkedList.clear();
+        timeLinkedList.endTimer(timeLinkedList.getStart());
+
+        timeLinkedList.totalTime();
+    }
 }

18152ee3c3a169e46ea5d78d14be87604c81158e
Owner <journeyjava0@gmail.com>
2020-11-27 20:24:23 -0700

    uploading hashmap exercise 3

--- src/labs_examples/datastructures/hashmap/labs/myHashMap/Timing/MyQueueTime.java
+++ src/labs_examples/datastructures/hashmap/labs/myHashMap/Timing/MyQueueTime.java
@@ -1,2 +1,27 @@
-package labs_examples.datastructures.hashmap.labs.myHashMap.Timing;public class MyQueueTime {
+package labs_examples.datastructures.hashmap.labs.myHashMap.Timing;
+
+import java.util.PriorityQueue;
+
+public class MyQueueTime {
+    public static void runTimeQueue() {
+    PriorityQueue<Integer> myPriorityQueue = new PriorityQueue<>();
+    Timer myQueueTime = new Timer("Queue");
+
+        myQueueTime.startTimer();
+        for (int i = 0; i < 100000; i++) {
+        myPriorityQueue.add(i);
+        }
+        myQueueTime.endTimer(myQueueTime.getStart());
+
+        myQueueTime.startTimer();
+        if(myPriorityQueue.contains(9999)){
+        myQueueTime.endTimer(myQueueTime.getStart());
+        }
+
+        myQueueTime.startTimer();
+        myPriorityQueue.clear();
+        myQueueTime.endTimer(myQueueTime.getStart());
+
+        myQueueTime.totalTime();
+    }
 }
